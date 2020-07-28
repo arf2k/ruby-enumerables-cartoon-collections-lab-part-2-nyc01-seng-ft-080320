f4a2cace7e521ed26a3d9f969eb859b8d498f39b
Alex Foreman <foreman.alexander@gmail.com>
2020-07-28 19:28:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,68 +1,17 @@
+
 def square_array(array)
-  array.map{|num| num**2}
+  array.map {|x| x**2}
 end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|calls| calls.capitalize + "!"}
 end
 
-def long_planeteer_calls(calls)
-  calls.find do |strings|
-    if strings.length > 4
-      return TRUE
-    else
-      return FALSE 
-    end
-  end
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|calls| calls.length > 4}
 end
-  
-def find_valid_calls(array)
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def square_array(array)
-#   array.map {|x| x**2}
-# end
-
-# def summon_captain_planet(planeteer_calls)
-#   planeteer_calls.map {|calls| calls.capitalize + "!"}
-# end
-
-# def long_planeteer_calls(planeteer_calls)
-#   planeteer_calls.any? {|calls| calls.length > 4}
-# end
-
-# def find_valid_calls(planeteer_calls)
-#   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-#   planeteer_calls.find {|calls| valid_calls.include?(calls)}
-# end
+def find_valid_calls(planeteer_calls)
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  planeteer_calls.find {|calls| valid_calls.include?(calls)}
+end
