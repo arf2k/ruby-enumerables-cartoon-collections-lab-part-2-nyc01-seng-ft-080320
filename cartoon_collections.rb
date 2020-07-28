4337c7a8a777c3be9d5ac05ed53494a65b4b3847
Alex Foreman <foreman.alexander@gmail.com>
2020-07-28 19:12:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,54 @@
-def square_array(array)
-  array.map {|x| x**2}
-end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|calls| calls.capitalize + "!"}
-end
 
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? {|calls| calls.length > 4}
-end
 
-def find_valid_calls(planeteer_calls)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|calls| valid_calls.include?(calls)}
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def square_array(array)
+#   array.map {|x| x**2}
+# end
+
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.map {|calls| calls.capitalize + "!"}
+# end
+
+# def long_planeteer_calls(planeteer_calls)
+#   planeteer_calls.any? {|calls| calls.length > 4}
+# end
+
+# def find_valid_calls(planeteer_calls)
+#   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+#   planeteer_calls.find {|calls| valid_calls.include?(calls)}
+# end
